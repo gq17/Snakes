e370735c554ab1ec2f66e3e6f86343c0a8d72239
gq17 <cauchy1017@gmail.com>
2016-03-25 16:26:13 +0100

    delete unnecessary

--- computeCurvature.m
+++ computeCurvature.m
@@ -9,17 +9,22 @@ function Q = computeCurvature(P)
 
 % By GUO Qiang 25/03/2016 at ENS
 
+xp = interp(P,4);
+
+
 Q = zeros(size(P,1), size(P,3));
 
 for i=1:size(P,3)
     dx = gradient(P(:,1,i));
     dy = gradient(P(:,2,i));
     Q(:,i) = gradient(atan2(dy,dx))./hypot(dx, dy);
+    
 end
 
 figure;
 for i=1:size(Q,2)
     plot(Q(:,i));
+    ylim([min(min(Q)) max(max(Q))]);
     title(['Curvature of contour of time: ', num2str(i)]);
     pause(1);
 end
